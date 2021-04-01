1a9ef50266a24310a040ac339f448b368eaac629
Bhavya Wahie <bhavyawahie@gmail.com>
2021-04-01 17:00:54 +0530

    The calculator requests were commented

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ app.get("/", function(req, res){
     res.send('<h1>Hello World</h1>');
 });
 
-app.get("/calc/add", (req,res) =>{
+/*app.get("/calc/add", (req,res) =>{
     let q1 = parseInt(req.query.q1);
     let q2 = parseInt(req.query.q2);
     let result = q1 + q2;
     res.send("Answer = " + result);
-});
+});*/
 
 app.listen(3000,function(){
     console.log("The server is running at port 3000");
